fe0e7405ea21d9250960ee421d7423685e4881ca
ykcaj-i <jliuarc@gmail.com>
2025-03-27 12:09:10 -0400

    main.js : changed JS variable and got rid of a function to reduce run time speed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 /**currently the variables are only adjusted and tweaked in js file**/
 let yearsInvested, startAmount, annualContributions, divYield, ytdGrowth;
 const yearEnd = []; //by year
-const yearEndFormatted = []; //format the array to be prettier
+const yearEndNums = []; //format the array to be prettier
 yearsInvested = 20; //years invested 
 startAmount = 100000; //starting amount of capital
 annualContributions = 25000; //amount contributted every year
@@ -23,36 +23,28 @@ function calculateTotalReturns(){
     let currentAmount = startAmount;   
     for(let i = 0; i < yearsInvested; i++){ 
         currentAmount = Math.round((currentAmount * ytdGrowth) + (calculateDividends(currentAmount)) + currentAmount) + annualContributions;
-        yearEnd.push(currentAmount);
+        yearEnd.push(currentAmount.toLocaleString()); //push currentAmount as strings
+        yearEndNums.push(currentAmount); //push currentAmount as numbers
     }
     console.log(yearEnd); //console log the array of strings
-    formatArray(); // call the comma function
+    console.log(yearEndNums);
 }
 
-function formatArray(){
-    let temp;
-    for(let i = 0; i < yearEnd.length; i++){
-        temp = yearEnd[i];
-        temp = temp.toLocaleString()
-        yearEndFormatted.push(temp);
-      
-    }
-    console.log(yearEndFormatted);
-}
 
 let count = "1";
 const date = new Date();
 let year = date.getFullYear();
-for(let i = 0; i < yearEndFormatted.length; i++){
+for(let i = 0; i < yearEnd.length; i++){
     document.getElementById('calculatedResult').innerHTML += //add html
     `<div class = "result">
         <span class = "year">year: 
             <span class = "count">${count}</span>&nbsp;|&nbsp;<span class = "fullYear">${year}</span></span>
             <br>
             <br>
-        <span class = "numbers">$&nbsp;${yearEndFormatted[i]}</span>
+        <span class = "numbers">$&nbsp;${yearEnd[i]}</span>
         <br>
     </div>`;
     count++;
     year++;
 }
+
---
+++ src/scripts/misc.js
@@ -0,0 +1,18 @@
+
+const finputs = [
+    {
+        USER_ID: JohnSmith,
+        yearInvested: 10,
+        startAmount: 100000,
+        annualContributions: 25000,
+        divYield: 0.0025,
+        ytdGrowth: 0.10,
+        bullMarket: true
+    }
+]
+
+
+const finputsJSON = JSON.stringify(finputs)
+
+const yearEnd = []; //by year
+const yearEndFormatted = []; //format the array to be prettier
